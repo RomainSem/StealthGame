258263d97c9529b4f884b7048d9fbbe7a18ca832
RomainSem <91747026+RomainSem@users.noreply.github.com>
2023-02-27 16:55:30 +0100

    added jump, started groundChecker

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -7,10 +7,16 @@ using UnityEngine.EventSystems;
 public class PlayerMovements : MonoBehaviour
 {
     #region Expose
+    [Header("Movements")]
     [SerializeField] float _joggingSpeed = 5;
     [SerializeField] float _runningSpeed = 8;
     [SerializeField] float _sneakingSpeed = 3;
-    [SerializeField] float _rotationSpeed = 3;
+    [SerializeField] float _rotationSpeed = 5;
+    [SerializeField] float _jumpForce = 5;
+
+    [Header("Floor Detection")]
+    [SerializeField] LayerMask _groundMask;
+    [SerializeField] Vector3 _boxDimension;
 
     #endregion
 
@@ -24,20 +30,30 @@ public class PlayerMovements : MonoBehaviour
     void Start()
     {
         _mainCamera = Camera.main;
+        _groundChecker = GameObject.Find("GroundChecker").transform;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-
+        Jump();
         _direction = _mainCamera.transform.forward * Input.GetAxis("Vertical") + _mainCamera.transform.right * Input.GetAxis("Horizontal");
         _direction *= _joggingSpeed;
+        Physics.OverlapBox(_groundChecker.position, _boxDimension, Quaternion.identity, _groundMask);
     }
 
     private void FixedUpdate()
     {
-        // Fait une chute normale
-        _direction.y = _rgdbody.velocity.y;
+        if (_isJumping)
+        {
+            _direction.y = _jumpForce;
+            _isJumping = false;
+        }
+        else
+        {
+            // Fait une chute normale
+            _direction.y = _rgdbody.velocity.y;
+        }
 
         RotateTowardsCamera();
         _rgdbody.velocity = _direction;
@@ -55,8 +71,17 @@ public class PlayerMovements : MonoBehaviour
             Vector3 lookDirection = _mainCamera.transform.forward;
             lookDirection.y = 0;
 
-            Quaternion rotation = Quaternion.LookRotation(lookDirection);
-            _rgdbody.MoveRotation(rotation);
+            Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
+            //Quaternion rotation = Quaternion.RotateTowards(_rgdbody.rotation, lookRotation, _rotationSpeed * Time.fixedDeltaTime);
+            _rgdbody.MoveRotation(lookRotation);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _isJumping = true;
         }
     }
 
@@ -66,6 +91,8 @@ public class PlayerMovements : MonoBehaviour
     Camera _mainCamera;
     Vector3 _direction = new Vector3();
     Rigidbody _rgdbody;
+    bool _isJumping = false;
+    Transform _groundChecker;
 
     #endregion
 }
